4e17dee0df3ddc23328eead16ef531e7dcdc7240
EricChiang01 <EricChiang365@gmail.com>
2023-05-15 20:00:32 -0700

    Final Changes

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Spam-Detection
+# Code Requirements
+
+To run this code, you need the following:
+1. Python: Make sure you have Python installed on your system. You can download it from the [official Python website:](https://www.python.org/downloads/)
+2. Required Libraries: This code requires the following libraries to be installed:
+    - pandas
+    - scikit-learn (sklearn)
+    - matplotlib
+    - seaborn
+You can install these libraries using pip, a package installer for Python. Run the following command to install the required libraries:
+```pip install pandas scikit-learn matplotlib seaborn```
+3. Dataset: The code assumes that you have a CSV file named "emails.csv" containing the email data. Make sure to place the file in the same directory as the Python script or specify the correct file path in the pd.read_csv() function call.
+
+# Code Functionality
+This code performs the following tasks:
+
+1. Reads and explores the email data from a CSV file.
+2. Splits the data into training and test sets.
+3. Converts the email text into a matrix of token counts using the CountVectorizer class from scikit-learn.
+4. Trains a Naive Bayes classifier on the training data.
+5. Performs cross-validation to evaluate the classifier's performance.
+6. Uses grid search to find the best hyperparameters for the classifier.
+7. Trains a new Naive Bayes classifier with the best hyperparameters.
+8. Predicts the class labels for the test set.
+9. Evaluates the classifier's performance by calculating accuracy and creating a confusion matrix.
+10. Visualizes the confusion matrix using a heatmap.
+11. Extracts the top spam and ham features based on their log probabilities.
+12. Displays the top 10 spam and ham features.
+13. Visualizes the top spam and ham features using bar plots.
+14. The code provides insights into the distribution of spam and ham emails, the accuracy of the classifier, the best hyperparameters found through grid search, and the most important features for classifying spam and ham emails.
+
+To run the code, execute it in a Python environment or run each section separately in a Jupyter Notebook or Python IDE. Make sure to have the required libraries and the dataset in the appropriate location.
--- detection.py
+++ detection.py
@@ -1,99 +1,116 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Load the data
-data = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip', sep='\t', header=None, names=['class', 'message'])
+df = pd.read_csv('emails.csv')
 
-# Preprocess the input messages
-data['message'] = data['message'].str.lower()
-data['message'] = data['message'].str.replace('[^\w\s]', '')
-data['message'] = data['message'].str.replace('\d+', '')
+# Explore and preprocess the data
+print("Data Summary:")
+print(df.head())
+print("\nNumber of emails:", len(df))
+print("Number of spam emails:", df['spam'].sum())
+print("Number of ham emails:", len(df) - df['spam'].sum())
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(data['message'], data['class'], test_size=0.2, random_state=42)
+df['spam'].value_counts().plot(kind='bar', color=['salmon', 'lightblue'])
+plt.title('Distribution of Spam and Ham Emails')
+plt.xlabel('Class')
+plt.ylabel('Count')
+plt.show()
 
-# Extract features from the messages
-vectorizer = CountVectorizer(stop_words='english')
-X_train_features = vectorizer.fit_transform(X_train)
-X_test_features = vectorizer.transform(X_test)
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(df['text'], df['spam'], test_size=0.2, random_state=42)
+
+# Convert emails to a matrix of token counts
+cv = CountVectorizer(stop_words='english')
+X_train_counts = cv.fit_transform(X_train)
+X_test_counts = cv.transform(X_test)
 
 # Train a Naive Bayes classifier
 clf = MultinomialNB()
-clf.fit(X_train_features, y_train)
 
-# Make predictions on the test set
-y_pred = clf.predict(X_test_features)
+# Perform cross-validation
+scores = cross_val_score(clf, X_train_counts, y_train, cv=5)
+mean_accuracy = scores.mean()
 
-# Evaluate the performance of the classifier
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred, pos_label='spam')
-recall = recall_score(y_test, y_pred, pos_label='spam')
-f1 = f1_score(y_test, y_pred, pos_label='spam')
+# Print the mean accuracy across folds
+print("Cross-Validation Accuracy: {:.2f}%".format(mean_accuracy * 100))
 
-print(f'Test Set Performance:')
-print(f'Accuracy: {accuracy:.2f}')
-print(f'Precision: {precision:.2f}')
-print(f'Recall: {recall:.2f}')
-print(f'F1-score: {f1:.2f}\n')
-
-# Get the indices of the spam messages
-spam_indices = data[data['class'] == 'spam'].index
-
-# Get the messages from the spam messages
-spam_messages = data.loc[spam_indices, 'message']
-
-# Extract features from the spam messages
-vectorizer = CountVectorizer(stop_words='english')
-spam_features = vectorizer.fit_transform(spam_messages)
-
-# Get the most common words in spam messages
-word_counts = spam_features.sum(axis=0)
-word_counts_df = pd.DataFrame(word_counts, columns=vectorizer.get_feature_names())
-word_counts_df = word_counts_df.T
-word_counts_df.columns = ['count']
-word_counts_df = word_counts_df.sort_values('count', ascending=False).head(10)
-
-# Plot the most common words in spam messages
-plt.bar(word_counts_df.index, word_counts_df['count'])
-plt.title('Most Common Words in Spam Messages')
-plt.xlabel('Word')
-plt.ylabel('Count')
+plt.plot(range(1, 6), scores*100, marker='o')
+plt.title('Cross-Validation Scores')
+plt.xlabel('Fold')
+plt.ylabel('Accuracy (%)')
 plt.show()
 
-# Get the most common sentences in spam messages
-sentence_counts = pd.Series(spam_messages).value_counts().head(10)
+# Define the parameter grid
+param_grid = {
+    'alpha': [0.1, 0.01, 0.001],
+    'fit_prior': [True, False]
+}
 
-# Plot the most common sentences in spam messages
-plt.bar(sentence_counts.index, sentence_counts.values)
-plt.title('Most Common Sentences in Spam Messages')
-plt.xticks(rotation=90)
-plt.xlabel('Sentence')
-plt.ylabel('Count')
+# Perform grid search
+grid_search = GridSearchCV(clf, param_grid, cv=5)
+grid_search.fit(X_train_counts, y_train)
+
+# Print the best hyperparameters and corresponding accuracy
+best_params = grid_search.best_params_
+best_accuracy = grid_search.best_score_
+print("Best Hyperparameters:", best_params)
+print("Best Accuracy: {:.2f}%".format(best_accuracy * 100))
+
+# Train a Naive Bayes classifier with the best hyperparameters
+clf = MultinomialNB(alpha=best_params['alpha'], fit_prior=best_params['fit_prior'])
+clf.fit(X_train_counts, y_train)
+
+# Predict on the test set
+predictions = clf.predict(X_test_counts)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, predictions)
+confusion_mat = confusion_matrix(y_test, predictions)
+
+# Print the evaluation metrics
+print("\nModel Evaluation:")
+print("Accuracy: {:.2f}%".format(accuracy * 100))
+print("Confusion Matrix:")
+print(confusion_mat)
+
+sns.heatmap(confusion_mat, annot=True, fmt='d', cmap='Blues')
+plt.title('Confusion Matrix')
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
 plt.show()
 
-# Train a Naive Bayes classifier on the entire dataset
-X_features = vectorizer.fit_transform(data['message'])
-y = data['class']
-clf = MultinomialNB()
-clf.fit(X_features, y)
+# Extract feature importance
+feature_names = cv.get_feature_names()
+top_spam_features = sorted(zip(feature_names, clf.feature_log_prob_[1]), key=lambda x: x[1], reverse=True)[:10]
+top_ham_features = sorted(zip(feature_names, clf.feature_log_prob_[0]), key=lambda x: x[1], reverse=True)[:10]
 
-# Get input from the user and preprocess it
-user_input = input("Enter a message: ")
-user_input = user_input.lower()
-user_input = user_input.replace('[^\w\s]', '')
-user_input = user_input.replace('\d+', '')
+print("\nTop 10 Spam Features:")
+for feature, log_prob in top_spam_features:
+    print(feature, "->", log_prob)
 
-# Extract features from the user input
-user_input_features = vectorizer.transform([user_input])
+print("\nTop 10 Ham Features:")
+for feature, log_prob in top_ham_features:
+    print(feature, "->", log_prob)
 
-# Make predictions on the user input
-prediction = clf.predict(user_input_features)[0]
+# Top Spam Features
+features, log_probs = zip(*top_spam_features)
+plt.barh(features, log_probs, color='salmon')
+plt.title('Top 10 Spam Features')
+plt.xlabel('Log Probability')
+plt.ylabel('Feature')
+plt.gca().invert_yaxis()
+plt.show()
 
-# Print the prediction
-if prediction == 'spam':
-    print("This message is spam.")
-else:
-    print("This message is not spam.")
+# Top Ham Features
+features, log_probs = zip(*top_ham_features)
+plt.barh(features, log_probs, color='lightblue')
+plt.title('Top 10 Ham Features')
+plt.xlabel('Log Probability')
+plt.ylabel('Feature')
+plt.gca().invert_yaxis()
+plt.show()
